cc15ea3d0dbd0ce61b1df7a40deaa802cbd3b3e1
limwoobin <drogba02@naver.com>
2022-06-25 14:09:14 +0900

    다원복

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,6 +1,5 @@
 package nextstep.subway.line.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
